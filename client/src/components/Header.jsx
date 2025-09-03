8ad93904f41fe22e42b04ebccf99f6a6033f7954
Your NameHuân <nguyenhuan26012004@gmail.com>
2025-09-03 17:11:27 +0700

    Sửa lại UI Header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -8,12 +8,12 @@ import {
 import Logo from "../assets/images/logo.svg";
 const Header = () => {
   return (
-    <header className="h-[70px] px-[10px] lg:px-[50px] lg:h-[100px] bg-white fixed top-0 left-0 z-999 transition-all duration-300 w-full shadow-[0px_8px_24px_rgba(150,158,166,0.2)] lg:max-w-[1620px] ">
-      <div className=" h-full flex items-center justify-between w-full">
+    <header className="h-[70px] px-[10px] lg:px-[50px] lg:h-[100px] bg-white fixed top-0 left-0 z-999 transition-all duration-300 w-full shadow-[0px_8px_24px_rgba(150,158,166,0.2)]  ">
+      <div className=" h-full lg:max-w-[1620px] mx-auto flex items-center justify-between w-full">
         <div className="lg:hidden">
           <BiMenuAltLeft size={22} />
         </div>
-        <div className="font-semibold text-[22px] ml-10 lg:hidden">
+        <div className="font-semibold text-[22px] flex flex-1 justify-center lg:hidden cursor-pointer">
           SSSTUTER
         </div>
         <div className="hidden lg:block">
@@ -24,7 +24,7 @@ const Header = () => {
             <li className="cursor-pointer">LIÊN HỆ</li>
           </ul>
         </div>
-        <div className="hidden lg:flex lg:items-center lg:justify-center lg:absolute w-full">
+        <div className="hidden lg:flex justify-center flex-1">
           <img src={Logo} alt="logo" />
         </div>
         <div className="flex items-center gap-[10px] lg:gap-9 ">
