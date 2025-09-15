fea92346973ef5c6d1e6f5fa5c065cb6aeff804d
Your NameHuân <nguyenhuan26012004@gmail.com>
2025-09-15 18:32:54 +0700

    Cấu hình BE

---
+++ server/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const PORT = 5000;
+productRoutes = require("./routes/product.routes");
+require("dotenv").config();
+const connectDB = require("./config/db");
+
+// Middleware để đọc JSON
+app.use(express.json());
+
+connectDB();
+
+// Route đơn giản
+app.get("/", (req, res) => {
+  res.send("Hello Node.js Backend!");
+});
+// Routes
+app.use("/api/products", productRoutes);
+// Server lắng nghe
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
