f79f17f5e3781b456d060cc692abf17654a7e50e
Your NameHuân <nguyenhuan26012004@gmail.com>
2025-10-01 00:03:31 +0700

    feat: Cập nhật định tuyến cho các trang mới

--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -5,8 +5,17 @@ import DefaultLayout from "../layouts/DefaultLayout";
 import AdminProductsManagement from "../admin/components/AdminProductsManagement";
 import AdminSidebar from "../admin/components/AdminSidebar";
 import AdminProductCreate from "../admin/components/AdminProductCreate";
-import Test from "../admin/components/test";
+// import Test from "../admin/components/test";
 import AdminProductEdit from "../admin/components/AdminProductEdit";
+import NotFound from "../shared/NotFound";
+import OrderManagement from "../admin/components/OrderManagement";
+import OrderDetail from "../admin/components/OrderDetail";
+import AdminLogin from "../admin/pages/AdminLogin";
+import AdminRegister from "../admin/pages/AdminRegister";
+import AdminWrapper from "../admin/components/Layout/AdminWrapper";
+import ProtectedRoute from "../admin/ProtectedRoute/ProtectedRoute";
+import ManageAdmins from "../admin/components/ManageAdmins";
+import GuestRoute from "../admin/components/GuestRoute/GuestRoute";
 const CartPage = lazy(() => import("../components/CartPage"));
 const HomePage = lazy(() => import("../pages/HomePage"));
 const CategoryPage = lazy(() => import("../components/CategoryPage"));
@@ -29,29 +38,86 @@ const AppRoutes = () => {
               path="contact"
               element={<div className="py-10">Liên hệ (đang cập nhật)</div>}
             />
+            <Route path="*" element={<NotFound />} />
           </Route>
 
           {/* ADMIN ROUTES */}
-          <Route>
-            <Route path="/admin" element={<AdminSidebar />} />
+          <Route path="/admin" element={<AdminWrapper />}>
+            <Route
+              path="login"
+              element={
+                <GuestRoute>
+                  <AdminLogin />
+                </GuestRoute>
+              }
+            />
+            <Route
+              path="register"
+              element={
+                <GuestRoute>
+                  <AdminRegister />
+                </GuestRoute>
+              }
+            />
+            <Route
+              index
+              element={
+                <ProtectedRoute>
+                  <AdminSidebar />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="products"
+              element={
+                <ProtectedRoute>
+                  <AdminProductsManagement />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="products/create"
+              element={
+                <ProtectedRoute>
+                  <AdminProductCreate />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="products/edit/:id"
+              element={
+                <ProtectedRoute>
+                  <AdminProductEdit />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="orders"
+              element={
+                <ProtectedRoute>
+                  <OrderManagement />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="orderDetail/:id"
+              element={
+                <ProtectedRoute>
+                  <OrderDetail />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/admin/manage-admins"
+              element={
+                <ProtectedRoute requiredRole="superadmin">
+                  <ManageAdmins />
+                </ProtectedRoute>
+              }
+            />
           </Route>
-          <Route path="/admin/products" element={<AdminProductsManagement />} />
-          <Route
-            path="/admin/products/create"
-            element={<AdminProductCreate />}
-          />
-          <Route
-            path="/admin/products/edit/:id"
-            element={<AdminProductEdit />}
-          />
-          <Route path="/admin/test" element={<Test />} />
+          {/* <Route path="/admin/test" element={<Test />} /> */}
           {/* 404 */}
-          <Route
-            path="*"
-            element={
-              <div className="py-10 text-center">Trang không tồn tại</div>
-            }
-          />
         </Routes>
       </Suspense>
     </CartProvider>
