dc4b98d83c7f5fa1ad1bed0d5bf4c0614976a91f
Your NameHuân <nguyenhuan26012004@gmail.com>
2025-10-01 00:03:16 +0700

    chore: Cập nhật cấu hình TailwindCSS

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -8,6 +8,11 @@ export default {
       fontFamily: {
         roboto: ["Roboto", "sans-serif"],
       },
+      className: {
+        inputClass: [
+          "w-full rounded-xl border border-slate-200 bg-white px-3 py-2 text-sm outline-none ring-sky-100 focus:border-sky-300 focus:ring",
+        ],
+      },
     },
   },
 
@@ -16,6 +21,14 @@ export default {
       addUtilities({
         ".nav-underline": {
           "@apply  before:content-[''] before:absolute before:left-1/2 before:bottom-[-2px] before:w-0 before:h-[2px] before:bg-black before:transition-all before:duration-300 before:ease-in-out before:transform before:-translate-x-1/2 before:origin-center hover:before:w-full":
+            {},
+        },
+      });
+    }),
+    plugin(function ({ addComponents }) {
+      addComponents({
+        ".inputClass": {
+          "@apply w-full rounded-xl border border-slate-200 bg-white px-3 py-2 text-sm outline-none ring-sky-100 focus:border-sky-300 focus:ring":
             {},
         },
       });
