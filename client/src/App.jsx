ca4ed6a784664645cd95904fd588d15fcff6be5b
Your NameHuân <nguyenhuan26012004@gmail.com>
2025-11-04 01:14:00 +0700

    chore: thêm React Query & axios, bọc App bằng QueryClientProvider

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,11 +2,17 @@ import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+const queryClient = new QueryClient();
 const App = () => {
   return (
     <BrowserRouter>
       <ToastContainer />
-      <AppRoutes />
+      <QueryClientProvider client={queryClient}>
+        <AppRoutes />
+        {/* <ReactQueryDevtools initialIsOpen={false} /> */}
+      </QueryClientProvider>
     </BrowserRouter>
   );
 };
