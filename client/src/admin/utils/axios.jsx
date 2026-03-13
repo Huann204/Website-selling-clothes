e3132b20c59e4b15bda7655af25a6fc4931a2df1
Your NameHuân <nguyenhuan26012004@gmail.com>
2026-03-13 16:45:21 +0700

    refactor: Comment out axios response interceptor for future implementation

--- client/src/admin/utils/axios.jsx
+++ client/src/admin/utils/axios.jsx
@@ -4,16 +4,16 @@ const api = axios.create({
   baseURL: API_URL,
   withCredentials: true,
 });
-api.interceptors.response.use(
-  (res) => res,
-  (error) => {
-    if (error.response?.status === 401) {
-      if (window.location.pathname !== "/admin/login") {
-        window.location.href = "/admin/login";
-      }
-    }
+// api.interceptors.response.use(
+//   (res) => res,
+//   (error) => {
+//     if (error.response?.status === 401) {
+//       if (window.location.pathname !== "/admin/login") {
+//         window.location.href = "/admin/login";
+//       }
+//     }
 
-    return Promise.reject(error);
-  },
-);
+//     return Promise.reject(error);
+//   },
+// );
 export default api;
